d7753d07a185da87a854e462ec305d5742beb5cf
Gerald Oskoboiny <gerald@w3.org>
2021-01-29 16:33:00 +0000

    Remove headings from expanded menus

--- web/navigation-v3/dist/assets/js/main.js
+++ web/navigation-v3/dist/assets/js/main.js
@@ -336,7 +336,7 @@ var navigation = function () {
       backButton.innerHTML = '<svg class="icon icon--larger" xmlns="http://www.w3.org/2000/svg" focusable="false" aria-hidden="true" viewBox="0 0 256 512" width="1em" height="1em"><path class="angle-right" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"/><path class="angle-left" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"/></svg>' + backText;
       fragment.appendChild(backButton);
       fragment.appendChild(clonedLink);
-      subNav.insertBefore(fragment, submenuFirstChild);
+//      subNav.insertBefore(fragment, submenuFirstChild);
       item.parentNode.replaceChild(toggleButton, item);
     });
 
