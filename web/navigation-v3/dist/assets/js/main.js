db107fbcc8ef7c87bdc33f3d2f24f4791424d025
Gerald Oskoboiny <gerald@w3.org>
2021-02-16 17:46:30 +0000

    Add Business areas as a separate menu

--- web/navigation-v3/dist/assets/js/main.js
+++ web/navigation-v3/dist/assets/js/main.js
@@ -329,7 +329,7 @@ var navigation = function () {
       toggleButton.setAttribute('type', 'button');
       toggleButton.setAttribute('aria-expanded', 'false');
       toggleButton.setAttribute('data-trigger', 'subnav');
-      toggleButton.innerHTML = linkText + '<svg class="icon nav-small" xmlns="http://www.w3.org/2000/svg" focusable="false" aria-hidden="true" viewBox="0 0 256 512" width="1em" height="1em"><path class="angle-right" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"/><path class="angle-left" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"/></svg><svg class="icon nav-wide" xmlns="http://www.w3.org/2000/svg" focusable="false" aria-hidden="true" viewBox="0 0 320 512" width="1em" height="1em"><path class="angle-down" d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"/><path class="angle-up" d="M177 159.7l136 136c9.4 9.4 9.4 24.6 0 33.9l-22.6 22.6c-9.4 9.4-24.6 9.4-33.9 0L160 255.9l-96.4 96.4c-9.4 9.4-24.6 9.4-33.9 0L7 329.7c-9.4-9.4-9.4-24.6 0-33.9l136-136c9.4-9.5 24.6-9.5 34-.1z"/></svg>';
+      toggleButton.innerHTML = linkText;
       backButton.setAttribute('type', 'button');
       backButton.setAttribute('class', 'button button--ghost u-full-width with-icon--before with-icon--larger');
       backButton.setAttribute('data-trigger', 'mobile-back');
