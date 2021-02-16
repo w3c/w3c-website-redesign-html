07215f10925a5278ad854795d582f02c40074a45
Nicola Saunders <nicola@studio24.net>
2021-02-16 12:26:30 +0000

    Update signed in header with latest JS

--- web/header-signedin/dist/assets/js/main.js
+++ web/header-signedin/dist/assets/js/main.js
@@ -443,7 +443,7 @@ var accountMenu = function () {
     toggleButton.setAttribute('class', 'button button--ghost with-icon--larger');
     toggleButton.setAttribute('data-trigger', 'account-menu');
     toggleButton.setAttribute('aria-expanded', 'false');
-    toggleButton.innerHTML = '<span class="sr-only">My account </span><picture class="avatar avatar--small"><img alt="" src="' + profile.avatar + '"/></picture>'; // Media query event handler
+    toggleButton.innerHTML = '<span class="sr-only">My account </span><div class="avatar avatar--small"><img alt="" src="' + profile.avatar + '"/></div>'; // Media query event handler
 
     var mq = window.matchMedia('(min-width: 71.25em)');
     mq.addListener(insertAccountBtn);
