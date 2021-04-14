af9d4d6d75b4338578673f03610a497a7ead9f75
Nicola Saunders <nicola@studio24.net>
2021-04-14 17:53:37 +0100

    Refactor carousel functionality to improve accessibility

--- apollo/assets/js/main/slider.js
+++ apollo/assets/js/main/slider.js
@@ -1,237 +1,307 @@
 /**
  * Content slider (carousel)
  */
+
 var contentSlider = (function () {
 
-	if ('IntersectionObserver' in window) {
+	// I18N
+	var sliderText;
+	var controlsText;
+	var prevText;
+	var nextText;
+	var slideText;
+	var ofText;
+	var activeDotText;
+	var selectedText;
 
-		// I18N
-		let sliderText;
-		let controlsText;
-		let prevText;
-		let nextText;
+	if (document.documentElement.lang === 'ja') {
 
-		if (document.documentElement.lang === 'ja') {
+		sliderText = 'スライダーの内容';
+		controlsText = 'スライダーコントロール';
+		prevText = '前のスライド';
+		nextText = '次のスライド';
+		slideText = 'スライド';
+		ofText = '/'
+		activeDotText = '（現在のアイテム）';
+		selectedText = ' 選択済み';
 
-			sliderText = 'スライダー';
-			controlsText = 'スライダーコントロール';
-			prevText = '前のスライド';
-			nextText = '次のスライド';
+	} else if (document.documentElement.lang === 'zh-hans') {
 
-		} else if (document.documentElement.lang === 'zh-hans') {
+		sliderText = '滑块内容';
+		controlsText = '滑块控件';
+		prevText = '上一张幻灯片';
+		nextText = '下一张幻灯片';
+		slideText = '幻灯片'
+		ofText = '之'
+		activeDotText = '（当前项）';
+		selectedText = ' 选定的';
 
-			sliderText = '滑杆';
-			controlsText = '滑块控件';
-			prevText = '上一张幻灯片';
-			nextText = '下一张幻灯片';
+	} else {
 
-		} else {
+		sliderText = 'slider content';
+		controlsText = 'slider controls';
+		prevText = 'previous slide';
+		nextText = 'next slide';
+		slideText = 'slide ';
+		ofText = ' of ';
+		activeDotText = '(current item)';
+		selectedText = ' selected';
 
-			sliderText = 'slider';
-			controlsText = 'slider controls';
-			prevText = 'previous slide';
-			nextText = 'next slide';
+	}
 
-		}
+	var slider = document.querySelector('[data-component="slider"] .l-center > div');
+	var dir = document.documentElement.getAttribute('dir');
 
-		let dir = document.documentElement.getAttribute('dir');
+	if (slider) {
 
-		const slider = document.querySelector('[data-component="slider"] .l-center > div');
+		var list = slider.querySelector('ul');
+		list.setAttribute('tabindex', '0');
+		list.setAttribute('aria-label', sliderText);
+		var slides = Array.prototype.slice.call(list.querySelectorAll('li'));
 
-		if (slider) {
+		// Add current class to first slide
+		slides[0].classList.add('js-current');
 
-			const list = slider.querySelector('ul');
-			const slides = list.querySelectorAll('li');
+		if (slides.length > 1) {
 
-			if (slides.length > 1) {
+			/**
+			 * Create previous and next button controls for slider
+			 * @return {HTMLUListElement}
+			 */
+			function createControls() {
 
-				slider.setAttribute('role', 'region');
-				slider.setAttribute('aria-label', sliderText);
-				slider.setAttribute('class', 'js-slider');
-				slider.setAttribute('tabindex', '0');
-
-				const observerSettings = {
-
-					root: slider,
-					rootMargin: '-10px'
-
-				}
-
-				const callback = function callback(slides, observer) {
-
-					Array.prototype.forEach.call(slides, function (entry) {
-
-						entry.target.classList.remove('visible');
-
-						if (!entry.intersectionRatio > 0) {
-
-							return;
-
-						}
-
-						entry.target.classList.add('visible');
-
-					});
-
-				};
-
-				const slideObserver = new IntersectionObserver(callback, observerSettings);
-
-				Array.prototype.forEach.call(slides, function (t) {
-
-					return slideObserver.observe(t);
-
-				});
-
-				let controls = document.createElement('ul');
+				var controls = document.createElement('ul');
 				controls.setAttribute('class', 'slider-controls');
 				controls.setAttribute('aria-label', controlsText);
 				controls.innerHTML = '<li><button class="button previous with-icon--larger" aria-label="' + prevText + '" style="padding:7px;"><svg class="icon icon--larger" xmlns="http://www.w3.org/2000/svg" focusable="false" aria-hidden="true" viewBox="0 0 256 512" width="1em" height="1em"><path class="angle-right" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"/><path class="angle-left" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"/></svg></button></li>'
 					+ '<li style="margin-top:0;"><button class="button next with-icon--larger" aria-label="' + nextText + '" style="padding:7px;"><svg class="icon icon--larger" xmlns="http://www.w3.org/2000/svg" focusable="false" aria-hidden="true" viewBox="0 0 256 512" width="1em" height="1em"><path class="angle-right" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"/><path class="angle-left" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"/></svg></button></li>';
-
-				let prev = controls.querySelector('.previous');
-				let next = controls.querySelector('.next');
 				controls.style.display = 'flex';
 				controls.style.justifyContent = 'space-between';
-				prev.disabled = true;
 
-				slider.parentNode.insertBefore(controls, slider.nextElementSibling);
+				return controls;
 
-				function scrollIt(slideToShow) {
+			}
 
-					let scrollPos = Array.prototype.indexOf.call(slides, slideToShow) * (slider.scrollWidth / slides.length);
+			/**
+			 * Create dot navigation for slider
+			 * @param slides
+			 * @return {HTMLUListElement}
+			 */
+			function createDotNav(slides) {
 
-					if (dir === 'rtl') {
-
-						slider.scrollLeft = -scrollPos;
-						return scrollPos;
-
-					} else {
-
-						slider.scrollLeft = scrollPos;
-						return scrollPos;
-
-					}
-
-				}
-
-				function showSlide(direction, slides) {
-
-					let visible = slider.querySelectorAll('.visible');
-					let i = direction === prev ? 0 : 1;
-
-					if (visible.length > 1) {
-
-						scrollIt(visible[i]);
-
-					} else {
-
-						let newSlide = i === 0 ? visible[0].previousElementSibling : visible[0].nextElementSibling;
-
-						if (newSlide) {
-
-							scrollIt(newSlide);
-
-						}
-
-					}
-
-				}
-
-				controls.addEventListener('click', function (e) {
-
-					showSlide(e.target.closest('button'), slides);
-
-				});
-
-				function disable() {
-
-					if (dir === 'rtl') {
-
-						prev.disabled = slider.scrollLeft === 0;
-						next.disabled = slider.scrollLeft === -(slider.scrollWidth - slider.offsetWidth);
-
-					} else {
-
-						prev.disabled = slider.scrollLeft < 1;
-						next.disabled = slider.scrollLeft === slider.scrollWidth - slider.offsetWidth;
-
-					}
-
-				}
-
-				//Debouncing for performance
-				let debounced;
-
-				slider.addEventListener('scroll', function () {
-
-					window.clearTimeout(debounced);
-					debounced = setTimeout(disable, 200);
-
-				});
-
-				function toggleSlide(self) {
-
-					// remove current class (if there is one set)
-					var currentDot = document.querySelector('.current');
-					if (currentDot) {
-						currentDot.classList.remove('current');
-					}
-
-					self.classList.add('current');
-
-				}
-
-				let slideNav = document.createElement('ul');
-				slideNav.setAttribute('class', 'slide-nav');
-				slideNav.setAttribute('class', 'clean-list');
-				slideNav.setAttribute('role', 'list');
-				slideNav.style.display = 'flex';
-				slideNav.style.justifyContent = 'center';
+				var dotNavContainer = document.createElement('ul');
+				dotNavContainer.setAttribute('class', 'slide-nav');
+				dotNavContainer.setAttribute('class', 'clean-list');
+				dotNavContainer.setAttribute('role', 'list');
+				dotNavContainer.style.display = 'flex';
+				dotNavContainer.style.justifyContent = 'center';
 
 				Array.prototype.forEach.call(slides, function (el, i) {
 
-					let li = document.createElement('li');
+					var li = document.createElement('li');
 					li.style.marginTop= '0';
-					let cssClass = (i===0) ? 'class="button current" ' : 'class="button " ';
-					let current = (i===0) ? ' <span class="visuallyhidden">(Current Item)</span>' : '';
-					li.innerHTML = '<button '+ cssClass +'data-slide="' + i + '"><span class="visuallyhidden">Slide</span> ' + '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon" focusable="false" aria-hidden="true" width="1em" height="1em"><defs/><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"/></svg>' + current + '</button>';
-					slideNav.appendChild(li);
+					var cssClass = (i===0) ? 'class="button js-current" ' : 'class="button " ';
+					var current = (i===0) ? ' <span class="visuallyhidden active-dot">' + activeDotText + '</span>' : '';
+					li.innerHTML = '<button '+ cssClass +'data-slide="' + i + '"><span class="visuallyhidden">' + slideText + (i + 1) + ofText + slides.length + '</span>' + '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon" focusable="false" aria-hidden="true" width="1em" height="1em"><defs/><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"/></svg>' + current + '</button>';
+					dotNavContainer.appendChild(li);
 
 				});
 
-				controls.parentNode.insertBefore(slideNav, controls.nextElementSibling);
+				return dotNavContainer;
 
-				document.addEventListener('click', function (event) {
+			}
 
-					if (event.target.matches('[data-slide]')) {
+			function createLiveRegion() {
 
-						toggleSlide(event.target);
+				var liveRegion = document.createElement('div');
+				liveRegion.setAttribute('role', 'status');
+				liveRegion.setAttribute('aria-live', 'polite');
+				liveRegion.setAttribute('class', 'visuallyhidden');
 
-						let scrollPos = event.target.getAttribute('data-slide') * (slider.scrollWidth / slides.length);
+				return liveRegion;
 
-						if (dir === 'rtl') {
+			}
 
-							slider.scrollLeft = -scrollPos;
-							return scrollPos;
+			var controls = createControls();
+			var prev = controls.querySelector('.previous');
+			var next = controls.querySelector('.next');
+			prev.disabled = true;
+			var dotNav = createDotNav(slides);
+			var dots = Array.prototype.slice.call(dotNav.querySelectorAll('.button'));
+			var liveRegion = createLiveRegion();
 
-						} else {
+			slider.setAttribute('class', 'js-slider');
+			slider.parentNode.insertBefore(controls, slider.nextElementSibling);
+			controls.parentNode.insertBefore(dotNav, controls.nextElementSibling);
+			dotNav.parentNode.insertBefore(liveRegion, dotNav.nextElementSibling);
 
-							slider.scrollLeft = scrollPos;
-							return scrollPos;
+			/**
+			 * Set slide positions, which are used in the switchSlide function
+			 */
+			function setSlidePositions() {
 
-						}
+				var slideWidth = slides[0].getBoundingClientRect().width;
+				for (var slide = 0; slide < slides.length; slide++) {
 
-					}
+					slides[slide].style.left = slideWidth * slide + 'px';
 
+				}
+
+			}
+
+			setSlidePositions();
+
+			/**
+			 * Switch between slides
+			 * @param {number} currentSlideIndex
+			 * @param {number} targetSlideIndex
+			 */
+			function switchSlide(currentSlideIndex, targetSlideIndex) {
+
+				var currentSlide = slides[currentSlideIndex];
+				var targetSlide = slides[targetSlideIndex];
+
+				// Switches to the correct slide
+				var destination = getComputedStyle(targetSlide).left;
+
+				if (dir === 'rtl') {
+
+					list.style.transform = 'translateX(' + destination + ')';
+
+				} else {
+
+					list.style.transform = 'translateX(-' + destination + ')';
+
+				}
+
+				currentSlide.classList.remove('js-current');
+				targetSlide.classList.add('js-current');
+
+				// Highlights the correct dot
+				var currentDot = dots[currentSlideIndex];
+				var targetDot = dots[targetSlideIndex];
+				currentDot.classList.remove('js-current');
+				var currentDotIndicator = currentDot.querySelector('.active-dot');
+				currentDotIndicator.remove();
+				targetDot.classList.add('js-current');
+				targetDot.innerHTML += '<span class="visuallyhidden active-dot">' + activeDotText + '</span>';
+
+				// Disable previous/next buttons
+				if (targetSlideIndex === 0) {
+
+					prev.setAttribute('disabled', true);
+					next.removeAttribute('disabled');
+
+				} else if (targetSlideIndex === dots.length - 1) {
+
+					prev.removeAttribute('disabled');
+					next.setAttribute('disabled', true);
+
+				} else {
+
+					prev.removeAttribute('disabled');
+					next.removeAttribute('disabled');
+
+				}
+
+				// Announce selected slide to screen reader
+				liveRegion.textContent = slideText + (targetSlideIndex + 1) + ofText + slides.length + selectedText;
+
+			}
+
+			/**
+			 * Get the current slide index
+			 * @return {number}
+			 */
+			function getCurrentSlideIndex() {
+
+				var currentSlide = list.querySelector('.js-current');
+				return slides.findIndex(function (slide) {
+					return slide === currentSlide;
 				});
 
 			}
+
+			next.addEventListener('click', function (event) {
+				var currentSlideIndex = getCurrentSlideIndex();
+				var nextSlideIndex = currentSlideIndex + 1;
+				switchSlide(currentSlideIndex, nextSlideIndex);
+			});
+
+			prev.addEventListener('click', function (event) {
+				var currentSlideIndex = getCurrentSlideIndex();
+				var previousSlideIndex = currentSlideIndex - 1;
+				switchSlide(currentSlideIndex, previousSlideIndex);
+			});
+
+			dotNav.addEventListener('click', function (event) {
+				var dot = event.target.closest('button');
+				if (!dot) return;
+				var currentSlideIndex = getCurrentSlideIndex();
+				var targetSlideIndex = dots.findIndex(function (d) {
+					return d === dot;
+				});
+				switchSlide(currentSlideIndex, targetSlideIndex);
+			});
+
+			list.addEventListener('keydown', function (event) {
+				var key = event.key;
+				if (key !== 'ArrowLeft' && key !== 'ArrowRight') return;
+
+				var currentSlideIndex = getCurrentSlideIndex();
+				var targetSlideIndex;
+
+				if (dir === 'rtl') {
+
+					if (key === 'ArrowRight') {
+
+						targetSlideIndex = currentSlideIndex - 1;
+
+					}
+
+					if (key === 'ArrowLeft') {
+
+						targetSlideIndex = currentSlideIndex + 1;
+
+					}
+
+				} else {
+
+					if (key === 'ArrowLeft') {
+
+						targetSlideIndex = currentSlideIndex - 1;
+
+					}
+
+					if (key === 'ArrowRight') {
+
+						targetSlideIndex = currentSlideIndex + 1;
+
+					}
+
+				}
+
+				if (targetSlideIndex < 0) {
+
+					targetSlideIndex = 0;
+
+				}
+
+				if (targetSlideIndex > slides.length - 1) {
+
+					targetSlideIndex = slides.length - 1;
+
+				}
+
+				switchSlide(currentSlideIndex, targetSlideIndex); // Focus on the correct slide
+
+			});
 
 		}
 
 	}
 
 })();
+
 
 export {contentSlider};
