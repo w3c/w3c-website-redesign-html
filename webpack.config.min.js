924cdbe5d9412f94d78ce0f4e1bab00b712cceb2
Nicola Saunders <nicola@studio24.net>
2021-06-07 18:54:49 +0100

    Refactor accessible autocomplete options

--- webpack.config.min.js
+++ webpack.config.min.js
@@ -1,7 +1,8 @@
 const path = require('path');
 module.exports = {
 	entry: {
-		'/web/dist/assets/js/s24-autocomplete': './apollo/assets/js/s24-autocomplete.js',
+		'/web/dist/assets/js/country-autocomplete': './apollo/assets/js/country-autocomplete.js',
+		'/web/dist/assets/js/working-group-autocomplete': './apollo/assets/js/working-group-autocomplete.js',
 		'/web/dist/assets/js/comments': './apollo/assets/js/comments.js',
 		'/web/dist/assets/js/main': './apollo/assets/js/main.js'
 	},
